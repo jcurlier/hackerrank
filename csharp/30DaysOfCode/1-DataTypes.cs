919dd6c465a7bc0e6117c8a5e6b2a79a6ee0fd06
Jerome Curlier <jcurlier@users.noreply.github.com>
2017-01-02 11:10:28 -0800

    Update 1-DataTypes.cs

--- csharp/30DaysOfCode/1-DataTypes.cs
+++ csharp/30DaysOfCode/1-DataTypes.cs
@@ -7,19 +7,15 @@ class Solution {
         int i = 4;
         double d = 4.0;
         string s = "HackerRank ";
-           
+        
         // Declare second integer, double, and String variables.
         int j = 0;
         double e = 0.0;
         string t = null;
 
         // Read and save an integer, double, and String to your variables.
-        string input = Console.ReadLine();
-        Int32.TryParse(input, out j);
-
-        input = Console.ReadLine();
-        Double.TryParse(input, out e);
-
+        j = Convert.ToInt32(Console.ReadLine());
+        e = Convert.ToDouble(Console.ReadLine());
         t = Console.ReadLine();
 
         // Print the sum of both integer variables on a new line.
