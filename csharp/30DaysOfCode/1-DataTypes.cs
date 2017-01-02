cd1de05a9f48431132a4e30aab5d4ce7b910cbfc
Jerome Curlier <jcurlier@users.noreply.github.com>
2017-01-02 10:37:02 -0800

    Update 1-DataTypes.cs

--- csharp/30DaysOfCode/1-DataTypes.cs
+++ csharp/30DaysOfCode/1-DataTypes.cs
@@ -1,3 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+           
         // Declare second integer, double, and String variables.
         int j = 0;
         double e = 0.0;
@@ -13,14 +23,13 @@
         t = Console.ReadLine();
 
         // Print the sum of both integer variables on a new line.
-        int sumInt = i + j;
-        Console.WriteLine($"{sumInt}");
+        Console.WriteLine($"{i + j}");
 
         // Print the sum of the double variables on a new line.
-        double sumDouble = d + e;
-        Console.WriteLine($"{sumDouble:F1}");
+        Console.WriteLine($"{(d + e):F1}");
 
         // Concatenate and print the String variables on a new line
         // The 's' variable above should be printed first.
-        string concatenated = s + t;
-        Console.WriteLine($"{concatenated}");
+        Console.WriteLine($"{(s + t)}");
+    }
+}
