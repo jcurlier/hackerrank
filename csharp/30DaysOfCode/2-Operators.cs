0e29f88b05f81a3aa06b9ce9b0a4060552451759
Jerome Curlier <jcurlier@users.noreply.github.com>
2017-01-02 11:06:17 -0800

    Update 2-Operators.cs

--- csharp/30DaysOfCode/2-Operators.cs
+++ csharp/30DaysOfCode/2-Operators.cs
@@ -4,24 +4,18 @@ class Solution
 {
     static void Main(String[] args)
     {
-        string input = null;
         double mealCost = 0.00;
         int tipPercent = 0;
         int taxPercent = 0;
         
-        input = Console.ReadLine();
-        Double.TryParse(input, out mealCost);
-        
-        input = Console.ReadLine();
-        Int32.TryParse(input, out tipPercent);
-        
-        input = Console.ReadLine();
-        Int32.TryParse(input, out taxPercent);
+        mealCost = Convert.ToDouble(Console.ReadLine());
+        tipPercent = Convert.ToInt32(Console.ReadLine());
+        taxPercent = Convert.ToInt32(Console.ReadLine());
         
         double tip = (mealCost * tipPercent) / 100;
         double tax = (mealCost * taxPercent) / 100;
 
         int totalCost = (int) Math.Round(mealCost + tip + tax, 0);
         Console.WriteLine($"The total meal cost is {totalCost} dollars.");
-    }
+    }   
 }
