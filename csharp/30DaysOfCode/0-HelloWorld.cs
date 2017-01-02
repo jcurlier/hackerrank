7176f27d4af7287ae594946e658c78cf7dc99cd3
Jerome Curlier <jcurlier@users.noreply.github.com>
2017-01-02 10:30:13 -0800

    Update 0-HelloWorld.cs

--- csharp/30DaysOfCode/0-HelloWorld.cs
+++ csharp/30DaysOfCode/0-HelloWorld.cs
@@ -1,15 +1,8 @@
-class Solution 
-{
+class Solution {
+    static void Main(String[] args) {
+        String inputString = Console.ReadLine(); 
 
-    static void Main(String[] args) 
-    {
-
-        String inputString; 
-        
-        inputString = Console.ReadLine(); 
-        
         Console.WriteLine("Hello, World.");
-        
         Console.WriteLine(inputString);
     }
 }
